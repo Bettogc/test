4921dcaaa14a64a73006b9c562f9c38b4204a1c0
Bettogc <betto793@gmail.com>
2015-11-30 06:55:08 -0300

    Fecha

--- Test/cloud/main.js
+++ Test/cloud/main.js
@@ -1,5 +1,32 @@
- Parse.Cloud.define("hello", function(request, response) {
-	 var f = new Date();
-	 
-	 response.success(f.toLocaleString(-0600 UTC));
- });
+// This call to external library of moment-timezone.js for select a country time.
+var moment = require('cloud/moment-timezone.js');
+// Contain the countries with Date/Time
+moment.tz.add(require('cloud/moment-timezone-with-data.js'));
+// Add (Date/Time) Guatemala country
+moment.tz.add('America/Guatemala|LMT CST CDT|62.4 60 50|0121212121|-24KhV.U 2efXV.U An0 mtd0 Nz0 ifB0 17b0 zDB0 11z0');
+
+Parse.Cloud.define("guatemalaDate", function (request, response) {  
+	
+	// Difference between the time zone Guatemala City and meridian Greenwich(.utcOffset('-06:00'))
+	var b = moment().tz("America/Guatemala");
+	// Date Format
+	//var b = b.format("DD-MM-YYYY hh:mm:ss a");
+
+	//response.success(b);
+	
+	
+	
+	var query = new Parse.Query("Hours");
+  query.find({
+    success: function(results) {
+
+    for (var i = 0; i < results.length; ++i) {
+		var ja = results[i].attributes.FinPromotion;
+    }
+		response.success(b.valueOf()+" "+ja.valueOf());
+	}
+  });
+	
+	
+	
+});
--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	  error: function(error) { }
 });
 	
-var a = moment.tz("America/Guatemala");
+/*var a = moment.tz("America/Guatemala");
 var b = moment.tz("May 12th 2014 8PM", "MMM Do YYYY hA", "America/Toronto");
 var c = moment.tz(1403454068850, "America/Toronto");
 a.format(); // 2013-11-18T11:55:00-05:00
@@ -21,20 +21,17 @@ b.format(); // 2014-05-12T20:00:00-04:00
 c.format(); // 2014-06-22T12:21:08-04:00
 	
 	var a = a.format('DD MM AAAA');
-
+*/
 	
-Parse.Cloud.run('hello', {}, {
+Parse.Cloud.run('guatemalaDate', {}, {
 success: function(result) {
-// result is 'Hello world!'
-	//alert(result)
+	//result is 'Hello world!'
+console.log(result)
+	
  },
 error: function(error) {
 	alert(error)
 }
 });
-	
-	
+		
 })
-
-
-// https://gist.github.com/raae/10534775
